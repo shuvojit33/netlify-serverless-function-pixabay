879b177fb145f130e6656867d40910747c44d5c7
Shuvojit Das <dasdesigndesk@gmail.com>
2023-03-04 00:48:57 +0600

    y2

--- dist/script.js
+++ dist/script.js
@@ -1,39 +1,45 @@
-const searchbar = document.querySelector(".search");
-const submitBtn = document.querySelector(".search-btn");
-const photoWrapper = document.querySelector(".photo-wrapper");
+// const searchbar = document.querySelector(".search");
+// const submitBtn = document.querySelector(".search-btn");
+// const photoWrapper = document.querySelector(".photo-wrapper");
 
-console.log("the first hello!")
+// // console.log("the first hello!")
 
-submitBtn.addEventListener("click", () => {
-  getPhoto(searchbar.value);
-  searchbar.value = "";
-  photoWrapper.innerHTML = "";
-});
+// submitBtn.addEventListener("click", () => {
+//   getPhoto(searchbar.value);
+//   searchbar.value = "";
+//   photoWrapper.innerHTML = "";
+// });
 
-window.addEventListener("keydown", (e) => {
-  if (e.keyCode === 13) {
-    getPhoto(searchbar.value);
-    searchbar.value = "";
-    photoWrapper.innerHTML = "";
-  }
-});
+// window.addEventListener("keydown", (e) => {
+//   if (e.keyCode === 13) {
+//     getPhoto(searchbar.value);
+//     searchbar.value = "";
+//     photoWrapper.innerHTML = "";
+//   }
+// });
 
-async function getPhoto(keyword) {
-  let apiURL = `/.netlify/functions/getPhotos?keyword=${keyword}`;
+// let pram2 = "test"
 
-  try {
-    const response = await fetch(apiURL, {
-      method: "GET",
-      headers: { accept: "application/json" },
-    });
-    const data = await response.json();
+// async function getPhoto(keyword) {
+//   let apiURL = `/.netlify/functions/getPhotos?q=${keyword}&pram2=${pram2}`;
 
-    for (let i = 0; i < data.imageURL.length; i++) {
-      let imageElement = document.createElement("img");
-      imageElement.setAttribute("src", `${data.imageURL[i].webformatURL}`);
-      photoWrapper.appendChild(imageElement);
-    }
-  } catch (error) {
-    alert(error);
-  }
-}
+//   // &q=Backgrounds&per_page=20&page=1&order=Popular&image_type=photo
+
+//   try {
+//     const response = await fetch(apiURL, {
+//       method: "GET",
+//       headers: { accept: "application/json" },
+//     });
+//     const data = await response.json();
+
+//     // console.log(data.hits);
+
+//     for (let i = 0; i < data.hits.length; i++) {
+//       let imageElement = document.createElement("img");
+//       imageElement.setAttribute("src", `${data.hits[i].webformatURL}`);
+//       photoWrapper.appendChild(imageElement);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
--- netlify/functions/getPhotos.js
+++ netlify/functions/getPhotos.js
@@ -4,27 +4,67 @@ const process = require("process");
 
 exports.handler = async (event, context) => {
   // export async function handler(event, context) {
-    console.log("hello!");
-  try {
-    const { keyword } = event.queryStringParameters;
-    let response = await axios.get(
-      `https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${keyword}&image_type=photo&safesearch=true&per_page=3`,
-      {
-        headers: { Accept: "application/json", "Accept-Encoding": "identity" },
-        params: { trophies: true },
-      }
-    );
+  // console.log("hello 2!");
 
-    let imageURL = response.data.hits;
+  try {
+    const { q } = event.queryStringParameters;
+    const { editors_choice } = event.queryStringParameters;
+    const { per_page } = event.queryStringParameters;
+
+    const { page } = event.queryStringParameters;
+    const { order } = event.queryStringParameters;
+    const { image_type } = event.queryStringParameters;
+    const { orientation } = event.queryStringParameters;
+
+    const { colors } = event.queryStringParameters;
+
+    let feedURL = `https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}`;
+
+    if (editors_choice) {
+      feedURL += `&editors_choice=${editors_choice}`;
+    }
+    if (q) {
+      feedURL += `&q=${q}`;
+    }
+    if (page) {
+      feedURL += `&page=${page}`;
+    }
+    if (order) {
+      feedURL += `&order=${order}`;
+    }
+    if (image_type) {
+      feedURL += `&image_type=${image_type}`;
+    }
+    if (orientation) {
+      feedURL += `&orientation=${orientation}`;
+    }
+
+    if (colors) {
+      feedURL += `&colors=${colors}`;
+    }
+
+    if (per_page) {
+      feedURL += `&per_page=${per_page}`;
+    }
+
+    // console.log("feedURL-" + feedURL);
+
+    let response = await axios.get(feedURL, {
+      headers: { Accept: "application/json", "Accept-Encoding": "identity" },
+      params: { trophies: true },
+    });
+
+    // let imageURL = response.data;
+    // console.log(response.data);
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ imageURL }),
+      body: JSON.stringify(response.data),
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error }),
+      // body: JSON.stringify({ error }),
     };
   }
 };
