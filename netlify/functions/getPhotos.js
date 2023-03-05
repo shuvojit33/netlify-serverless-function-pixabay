81e805c520adbb922c668986b923bd2dcbd6994c
Shuvojit Das <dasdesigndesk@gmail.com>
2023-03-05 21:01:33 +0600

    kk

--- netlify/functions/getPhotos.js
+++ netlify/functions/getPhotos.js
@@ -2,32 +2,15 @@ require("dotenv").config();
 const axios = require("axios");
 const process = require("process");
 
-
-// //This solves the "No ‘Access-Control-Allow-Origin’ header is present on the requested resource."
-// let HEADERS = {
-//   'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin',
-//   'Content-Type': 'application/json', //optional
-//   'Access-Control-Allow-Methods': 'POST, OPTIONS',
-//   'Access-Control-Max-Age': '8640'
-// }
-// HEADERS['Access-Control-Allow-Origin'] = '*'
-// HEADERS['Vary'] = 'Origin'
-
-
 exports.handler = async (event, context) => {
-  // export async function handler(event, context) {
-  // console.log("hello 2!");
-
   try {
     const { q } = event.queryStringParameters;
     const { editors_choice } = event.queryStringParameters;
     const { per_page } = event.queryStringParameters;
-
     const { page } = event.queryStringParameters;
     const { order } = event.queryStringParameters;
     const { image_type } = event.queryStringParameters;
     const { orientation } = event.queryStringParameters;
-
     const { colors } = event.queryStringParameters;
 
     let feedURL = `https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}`;
@@ -59,8 +42,6 @@ exports.handler = async (event, context) => {
       feedURL += `&per_page=${per_page}`;
     }
 
-    // console.log("feedURL-" + feedURL);
-
     let response = await axios.get(feedURL, {
       headers: { Accept: "application/json", "Accept-Encoding": "identity" },
       params: { trophies: true },
@@ -72,9 +53,7 @@ exports.handler = async (event, context) => {
       headers: {
         "access-control-allow-origin": "*",       
       },
-    };
-
-   
+    };   
   } catch (error) {
     return {
       statusCode: 500,
@@ -82,5 +61,3 @@ exports.handler = async (event, context) => {
     };
   }
 };
-
-// module.exports.handler();
